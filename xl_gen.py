1a99294c3d3b1c3bae91b0150833932941655c92
kushjain@thoughtworks.com <Mr.KushJain@gmail.com>
2013-12-06 19:38:39 +0530

    Added conditional formating for average marks greater than 50

--- xl_gen.py
+++ xl_gen.py
@@ -6,23 +6,31 @@ worksheet = workbook.add_worksheet()
 
 result = (
     ['Ram', 96],
-    ['Ravi',   99],
-    ['Kush',  95],
-    ['Shyam',    50],
+    ['Ravi',   2],
+    ['Kush',  5],
+    ['Shyam',    5],
 )
 
 row = 0
 col = 0
+sum = 0
 
 # Iterate over the data and write it out row by row.
 for name, marks in (result):
     worksheet.write(row, col,     name)
     worksheet.write(row, col + 1, marks)
+    sum += marks
     row += 1
-yellow_background = workbook.add_format()
 
-yellow_background.set_bg_color('yellow')
+no_of_students = len(result)
+average = float(sum)/no_of_students
+
+background = workbook.add_format()
+if average > 50:
+	background.set_bg_color('yellow')
+else:
+	background.set_bg_color('red')
 worksheet.write(row, 0, 'Average')
-worksheet.write(row, 1, '=AVERAGE(B1:B4)',yellow_background)
+worksheet.write(row, 1, average,background)
 
 workbook.close()
